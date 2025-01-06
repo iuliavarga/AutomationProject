70163121bf9686f2596a5c3d6c48bb217ebce092
julia varga <varga_jh@yahoo.com>
2025-01-06 16:26:50 +0200

    adaugare parametrii text box si webelement

--- AutomationProject/UnitTest1.cs
+++ AutomationProject/UnitTest1.cs
@@ -1,25 +1,38 @@
+using System.Runtime.Intrinsics.X86;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 
 namespace AutomationProject
 {
     public class TextBoxTest
     {
        
-        IWebDriver driver;
+        IWebDriver driver;     
+                
+        public void DefinireParametriiTextBox(IWebElement elementsFirstName, IWebElement elementsEmail, IWebElement elementsAdresa, IWebElement elementsPermanentAdresa )
+        {
+            
+            elementsFirstName.SendKeys("varga");
+            elementsEmail.SendKeys("vjh@yahoo.com");
+            elementsAdresa.SendKeys("str. Calea Dorobantilor 89");
+            elementsPermanentAdresa.SendKeys("str. Calea Dorobantilor 1, CJ");
+
+        }
 
 
         [Test]
-        public void Test1()
+        public void TextBox()
         {
            
             driver = new ChromeDriver();
-
+           
             driver.Navigate().GoToUrl("https://demoqa.com/");
             driver.Manage().Window.Maximize();
 
             IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
             js.ExecuteScript("window.scrollTo(0,1000)");
+
 
             IWebElement elementsButton = driver.FindElement(By.XPath("//h5[text()='Elements']"));
             elementsButton.Click();
@@ -28,21 +41,46 @@ namespace AutomationProject
             elementsTextBoxButton.Click();
 
             IWebElement elementsFirstName = driver.FindElement(By.Id("userName"));
-            elementsFirstName.SendKeys("varga julia");
-
+            //elementsFirstName.SendKeys("varga julia");
+            
             IWebElement elementsEmail = driver.FindElement(By.Id("userEmail"));
-            elementsEmail.SendKeys("vjh@yahoo.com");
+           // elementsEmail.SendKeys("vjh@yahoo.com");
 
             IWebElement elementsAdresa = driver.FindElement(By.Id("currentAddress"));
-            elementsAdresa.SendKeys("str. Calea Dorobantilor 89");
+            //elementsAdresa.SendKeys("str. Calea Dorobantilor 89");
 
             IWebElement elementsPermanentAdresa = driver.FindElement(By.Id("permanentAddress"));
-            elementsPermanentAdresa.SendKeys("str. Calea Dorobantilor 1, CJ");
+            //elementsPermanentAdresa.SendKeys("str. Calea Dorobantilor 1, CJ");
 
-            IWebElement elementsSubmitButton = driver.FindElement(By.XPath("//button[text()='Submit']"));
-            elementsSubmitButton.Click();
+            DefinireParametriiTextBox(elementsFirstName, elementsEmail, elementsPermanentAdresa, elementsPermanentAdresa);
+        
+
+            IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
+
+            IWebElement elementsSubmitButton = driver.FindElement(By.Id("submit"));
+            //driver.FindElement(By.XPath("//button[text()='Submit']"));
+            //elementsSubmitButton.Submit();
+
+            jse.ExecuteScript("arguments[0].click();", elementsSubmitButton);
 
         }
+
+
+        public void DefinireParametriiWebTables(IWebElement popFirstName, IWebElement popLastName, IWebElement popUserEmail,IWebElement popAge, IWebElement popSalary, IWebElement popDepartment)
+        {
+
+
+            popFirstName.SendKeys("J");
+            popLastName.SendKeys("V");
+            popUserEmail.SendKeys("vjs@yahoo.com");
+            popAge.SendKeys("44");
+            popSalary.SendKeys("1000");
+            popDepartment.SendKeys("it");
+
+        }
+
+
+
 
         [Test]
 
@@ -73,38 +111,56 @@ namespace AutomationProject
             IWebElement popSalary = driver.FindElement(By.Id("salary"));
             IWebElement popDepartment = driver.FindElement(By.Id("department"));
 
-            popFirstName.SendKeys("Julia");
-            popLastName.SendKeys("Varga");
-            popUserEmail.SendKeys("vjs@yahoo.com");
-            popAge.SendKeys("44");
-            popSalary.SendKeys("1000");
-            popDepartment.SendKeys("it");
+
+            //popFirstName.SendKeys(firstName);
+            //popLastName.SendKeys(lastName);
+            //popUserEmail.SendKeys(userEmail);
+            //popAge.SendKeys(age);
+            //popSalary.SendKeys(salary);
+            //popDepartment.SendKeys(department);
+
+
+            //popFirstName.SendKeys("Julia");
+            //popLastName.SendKeys("Varga");
+            //popUserEmail.SendKeys("vjs@yahoo.com");
+            //popAge.SendKeys("44");
+            //popSalary.SendKeys("1000");
+            //popDepartment.SendKeys("it");
+
+            DefinireParametriiWebTables(popFirstName, popLastName, popUserEmail, popAge, popSalary, popDepartment);
+
 
             IJavaScriptExecutor jse = (IJavaScriptExecutor)driver;
-
 
             IWebElement elementsSubmitButton = driver.FindElement(By.Id("submit"));
 
             jse.ExecuteScript("arguments[0].click();", elementsSubmitButton);
 
+
+
+
+
+
+            // jse.ExecuteScript("arguments[0].click();", elementsSubmitButton);
+
             //elementsSubmitButton.Submit();
 
-            IWebElement newRowWebTable = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]"));
-            Console.WriteLine(newRowWebTable.Text);
+            //IWebElement newRowWebTable = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]"));
+            //Console.WriteLine(newRowWebTable.Text);
 
-            IWebElement firstName = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][1]"));
-            IWebElement lastName = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][2]"));
-            IWebElement age = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][3]"));
-            IWebElement email = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][4]"));
-            IWebElement salary = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][5]"));
-            IWebElement department = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][6]"));
+            //IWebElement firstName = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][1]"));
+            //IWebElement lastName = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][2]"));
+            //IWebElement age = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][3]"));
+            //IWebElement email = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][4]"));
+            //IWebElement salary = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][5]"));
+            //IWebElement department = driver.FindElement(By.XPath("//div[@class='rt-tr-group'][4]//div[@class='rt-td'][6]"));
 
-            Assert.That(firstName.Text.Equals("Julia"));
-            Assert.That(lastName.Text.Equals("Varga"));
-            Assert.That(age.Text.Equals("44"));
-            Assert.That(email.Text.Equals("vjs@yahoo.com"));
-            Assert.That(salary.Text.Equals("1000"));
-            Assert.That(department.Text.Equals("it"));
+            //Assert.That(firstName.Text.Equals("Julia"));
+            //Assert.That(lastName.Text.Equals("Varga"));
+            //Assert.That(age.Text.Equals("44"));
+            //Assert.That(email.Text.Equals("vjs@yahoo.com"));
+            //Assert.That(salary.Text.Equals("1000"));
+            //Assert.That(department.Text.Equals("it"));
 
             //Assert.True(newRowWebTable.Text.Contains("Julia"));
             //Assert.True(newRowWebTable.Text.Contains("Varga"));
@@ -126,12 +182,12 @@ namespace AutomationProject
 
 
 
-        //    [TearDown]
-        //    public void TearDown()
-        //    {
-        //        driver.Quit();
-        //        driver.Close();
-        //    }
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    driver.Quit();
+        //    driver.Close();
+        //}
 
     }
 }
