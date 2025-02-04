d173c47ee2232c7fd0a9d5413b89cd6c6835ec01
julia varga <varga_jh@yahoo.com>
2025-02-04 14:55:47 +0200

    tema selectable

---
+++ AutomationProject/Frames.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace AutomationProject
+{
+    public class Frames
+    {
+
+        IWebDriver driver;
+
+        [Test]
+        public void ExecFrames()
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            driver.Manage().Window.Maximize();
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollTo(0,1000)");
+
+            IWebElement alertFramesButtom = driver.FindElement(By.XPath("//div[@class='card mt-4 top-card'][3]"));
+            alertFramesButtom.Click();
+
+            List<IWebElement> listFrames = driver.FindElements(By.XPath("//div[@class='element-list collapse show']//li[@class='btn btn-light ']")).ToList();
+            listFrames[2].Click();
+            
+            IWebElement frame1 = driver.FindElement(By.Id("frame1"));
+
+            driver.SwitchTo().Frame(frame1);
+
+            IWebElement frameText = driver.FindElement(By.Id("sampleHeading"));
+            Console.WriteLine($"text is:  {frameText.Text}");
+
+            driver.SwitchTo().DefaultContent();
+            IWebElement frame2 = driver.FindElement(By.Id("frame2"));
+            driver.SwitchTo().Frame(frame2);
+           
+            IWebElement frameText2 = driver.FindElement(By.Id("sampleHeading"));
+            Console.WriteLine($"text is:  {frameText2.Text}");
+
+
+
+        }
+
+
+    }
+}
---
+++ AutomationProject/Windows.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace AutomationProject
+{
+    public class Windows
+    {
+
+        IWebDriver driver;
+
+        [Test]
+        public void ExecWindows()
+        {
+            driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            driver.Manage().Window.Maximize();
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("window.scrollTo(0,1000)");
+
+            IWebElement alertFramesButtom = driver.FindElement(By.XPath("//div[@class='card mt-4 top-card'][3]"));
+            alertFramesButtom.Click();
+
+            List<IWebElement> listFrames = driver.FindElements(By.XPath("//div[@class='element-list collapse show']//li[@class='btn btn-light ']")).ToList();
+            listFrames[0].Click();
+
+            IWebElement newtabButton = driver.FindElement(By.Id("tabButton"));
+            newtabButton.Click();
+
+            List<string> tabList = new List<string>(driver.WindowHandles);
+            driver.SwitchTo().Window(tabList[1]);
+            
+            IWebElement textNewTab = driver.FindElement(By.Id("sampleHeading"));
+            Console.WriteLine($"text is:  {textNewTab.Text}");
+
+            driver.Close();
+            driver.SwitchTo().Window(tabList[0]);
+
+            IWebElement newWindowButton = driver.FindElement(By.Id("windowButton"));
+            newWindowButton.Click();
+
+            List<string> windowList = new List<string>(driver.WindowHandles);
+
+            driver.SwitchTo().Window(windowList[1]);
+
+            IWebElement textNewWindow = driver.FindElement(By.Id("sampleHeading"));
+            Console.WriteLine($"text is:  {textNewWindow.Text}");
+
+
+
+
+        }
+    }
+}
